9619ec1bac4803531b167e71115cca4f805d96eb
ykan <nameyukan@gmail.com>
2021-12-01 11:00:07 +0800

    feat: order list

---
+++ src/createOrderList.test.ts
@@ -0,0 +1,264 @@
+import {
+  addItemAfterId, addItemBeforeId, createList, createOrderList, isList, LinkNode
+} from './createOrderList';
+
+describe('createList push method', () => {
+  test('push', () => {
+    const list = createList()
+    const item1: LinkNode = {
+      id: 1,
+    }
+    list.push(item1)
+    expect(list.tail).toBe(item1)
+    const item2: LinkNode = {
+      id: 3,
+    }
+    list.push(item2)
+    expect(list.tail).toBe(item2)
+    expect(list.head).toBe(item1)
+
+    const item3: LinkNode = {
+      id: 4,
+    }
+    list.push(item3)
+    expect(list.tail).toBe(item3)
+    expect(list.tail?.prev).toBe(item2)
+  })
+
+  test('push list with only head', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    const list2 = createList()
+    list2.pushList(list1)
+    expect(list2.tail).toBe(list1.head)
+  })
+
+  test('push list with only tail', () => {
+    const list1 = createList()
+    list1.push({
+      id: 1,
+    })
+    const list2 = createList()
+    list2.pushList(list1)
+    expect(list2.tail).toBe(list1.tail)
+  })
+
+  test('push list when list is empty', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    const list2 = createList()
+    list2.pushList(list1)
+    expect(list2.tail).toBe(list1.tail)
+    expect(list2.head).toBe(list1.head)
+  })
+  test('push list when list only has head', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    const list2 = createList({
+      id: 3,
+    })
+    list2.pushList(list1)
+    expect(list2.head?.id).toBe(3)
+    expect(list2.tail?.id).toBe(2)
+  })
+  test('push list when list only has tail', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    const list2 = createList()
+    list2.push({
+      id: 3,
+    })
+    list2.pushList(list1)
+    expect(list2.head?.id).toBe(3)
+    expect(list2.tail?.id).toBe(2)
+  })
+})
+
+describe('createList unshift method', () => {
+  test('unshift', () => {
+    const list = createList()
+    const item1: LinkNode = {
+      id: 1,
+    }
+    list.unshift(item1)
+    expect(list.head).toBe(item1)
+    const item2: LinkNode = {
+      id: 3,
+    }
+    list.unshift(item2)
+    expect(list.head).toBe(item2)
+    expect(list.tail).toBe(item1)
+
+    const item3: LinkNode = {
+      id: 4,
+    }
+    list.unshift(item3)
+    expect(list.head).toBe(item3)
+    expect(list.head?.next).toBe(item2)
+  })
+
+  test('unshift list with only head', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    const list2 = createList()
+    list2.unshiftList(list1)
+    expect(list2.head).toBe(list1.head)
+  })
+
+  test('unshift list with only tail', () => {
+    const list1 = createList()
+    list1.push({
+      id: 1,
+    })
+    const list2 = createList()
+    list2.unshiftList(list1)
+    expect(list2.head).toBe(list1.tail)
+  })
+
+  test('unshift list when list is empty', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    const list2 = createList()
+    list2.unshiftList(list1)
+    expect(list2.tail).toBe(list1.tail)
+    expect(list2.head).toBe(list1.head)
+  })
+
+  test('unshift list when list only has head', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    const list2 = createList({
+      id: 3,
+    })
+    list2.unshiftList(list1)
+    expect(list2.head?.id).toBe(1)
+    expect(list2.tail?.id).toBe(3)
+  })
+  test('unshift list when list only has tail', () => {
+    const list1 = createList({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    const list2 = createList({
+      id: 3,
+    })
+    list2.unshiftList(list1)
+    expect(list2.head?.id).toBe(1)
+    expect(list2.tail?.id).toBe(3)
+  })
+})
+
+describe('create list with node', () => {
+  test('init node is tail', () => {
+    const list1 = createList()
+    list1.push({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    list1.push({
+      id: 3,
+    })
+
+    const list2 = createList(list1.tail)
+    expect(list2.head?.id).toBe(1)
+    expect(list2.tail?.id).toBe(3)
+  })
+
+  test('init node is head', () => {
+    const list1 = createList()
+    list1.push({
+      id: 1,
+    })
+    list1.push({
+      id: 2,
+    })
+    list1.push({
+      id: 3,
+    })
+
+    const list2 = createList(list1.head)
+    expect(list2.head?.id).toBe(1)
+    expect(list2.tail?.id).toBe(3)
+  })
+})
+
+describe('isList', () => {
+  test('case 1', () => {
+    const node1: LinkNode = {
+      id: 1,
+    }
+    const node2: LinkNode = {
+      id: 3,
+    }
+    const node3: LinkNode = {
+      id: 3,
+    }
+    node2.prev = node3
+    node3.next = node2
+    expect(isList(node1)).toBe(false)
+    expect(isList(node2)).toBe(true)
+  })
+})
+
+describe('addItemBeforeId', () => {
+  test('case 1', () => {
+    const inputArr: LinkNode[] = [{ id: 1 }, { id: 2, before: 1 }, { id: 3, before: 1 }]
+    addItemBeforeId(inputArr, inputArr[1], 1)
+    expect(inputArr[0].prev).toBe(inputArr[1])
+    addItemBeforeId(inputArr, inputArr[2], 1)
+    expect(inputArr[1].next).toBe(inputArr[2])
+  })
+})
+
+describe('addItemAfterId', () => {
+  test('case 1', () => {
+    const inputArr: LinkNode[] = [{ id: 1 }, { id: 2, before: 1 }, { id: 3, before: 1 }]
+    addItemAfterId(inputArr, inputArr[1], 1)
+    expect(inputArr[0].next).toBe(inputArr[1])
+    addItemAfterId(inputArr, inputArr[2], 1)
+    expect(inputArr[1].prev).toBe(inputArr[2])
+  })
+})
+
+describe('createOrderList', () => {
+  test('case 1', () => {
+    const list = createOrderList([
+      { id: 1 },
+      { id: 2, before: 1 }, // 这里 before 的意思是自己要排在 id 为 1 的元素前面
+      { id: 3, after: 1 }, // 这里 after 的意思是自己要排在 id 为 1 元素后面
+      { id: 5, first: true },
+      { id: 6, last: true },
+      { id: 7, after: 8 }, // 这里 after 的意思是自己要排在 id 为 8 元素后面
+      { id: 8 },
+      { id: 9 },
+    ])
+    const result = list.toArray().map((item) => item.id)
+    expect(result).toEqual([5, 2, 1, 3, 8, 7, 9, 6])
+  })
+})
--- src/createOrderList.ts
+++ src/createOrderList.ts
@@ -1,6 +1,284 @@
-export function createOrderList() {
-  const instance = {
-    add(items: []) {},
+export interface LinkNode {
+  next?: LinkNode
+  prev?: LinkNode
+
+  id: number
+  before?: number
+  after?: number
+  first?: boolean
+  last?: boolean
+}
+
+export interface List {
+  readonly head?: LinkNode
+  readonly tail?: LinkNode
+  push: (item: LinkNode) => void
+  unshift: (item: LinkNode) => void
+  pushList: (list: List) => void
+  unshiftList: (list: List) => void
+  refresh: () => void
+  toArray: () => LinkNode[]
+}
+
+export function createList(initNode?: LinkNode): List {
+  let innerHead: LinkNode | undefined
+  let innerTail: LinkNode | undefined
+
+  function refreshHeadAndTail(node: LinkNode) {
+    if (node.prev && node.next) {
+      innerHead = node.prev
+      while (innerHead.prev) {
+        innerHead = innerHead?.prev
+      }
+      innerTail = node.next
+      while (innerTail.next) {
+        innerTail = innerTail?.next
+      }
+    } else if (node.prev) {
+      innerTail = node
+      innerHead = node.prev
+      while (innerHead.prev) {
+        innerHead = innerHead?.prev
+      }
+    } else if (node.next) {
+      innerHead = node
+      innerTail = node.next
+      while (innerTail.next) {
+        innerTail = innerTail?.next
+      }
+    }
+  }
+  const instance: List = {
+    get head() {
+      return innerHead
+    },
+    get tail() {
+      return innerTail
+    },
+    push(item) {
+      if (innerTail) {
+        innerTail.next = item
+        item.prev = innerTail
+        if (!innerTail.prev && !innerHead) {
+          innerHead = innerTail
+        } else if (!innerTail.prev && innerHead) {
+          innerTail.prev = innerHead
+          innerHead.next = innerTail
+        }
+      }
+
+      innerTail = item
+    },
+    unshift(item) {
+      if (innerHead) {
+        innerHead.prev = item
+        item.next = innerHead
+        if (!innerHead.next && !innerTail) {
+          innerTail = innerHead
+        } else if (!innerHead.next && innerTail) {
+          innerHead.next = innerTail
+          innerTail.prev = innerHead
+        }
+      }
+      innerHead = item
+    },
+    pushList(list) {
+      if (list.head && list.tail) {
+        if (!innerHead && !innerTail) {
+          innerHead = list.head
+          innerTail = list.tail
+        } else if (innerHead && !innerTail) {
+          innerHead.next = list.head
+          list.head.prev = innerHead
+          innerTail = list.tail
+        } else if (innerTail && !innerHead) {
+          innerTail.next = list.head
+          list.head.prev = innerTail
+          innerHead = innerTail
+          innerTail = list.tail
+        } else if (innerTail && innerHead) {
+          innerTail.next = list.head
+          list.head.prev = innerTail
+          innerTail = list.tail
+        }
+      } else {
+        const node = list.head || list.tail
+        if (node) {
+          instance.push(node)
+        }
+      }
+    },
+    unshiftList(list) {
+      if (list.head && list.tail) {
+        if (!innerHead && !innerTail) {
+          innerHead = list.head
+          innerTail = list.tail
+        } else if (innerHead && !innerTail) {
+          innerHead.prev = list.tail
+          list.tail.next = innerHead
+          innerTail = innerHead
+          innerHead = list.head
+        } else if (innerTail && !innerHead) {
+          innerTail.prev = list.tail
+          list.tail.next = innerTail
+          innerHead = list.head
+        } else if (innerTail && innerHead) {
+          innerHead.prev = list.tail
+          list.tail.next = innerHead
+          innerHead = list.head
+        }
+      } else {
+        const node = list.head || list.tail
+        if (node) {
+          instance.unshift(node)
+        }
+      }
+    },
+    refresh() {
+      if (innerHead && !innerTail) {
+        refreshHeadAndTail(innerHead)
+      } else if (!innerHead && innerTail) {
+        refreshHeadAndTail(innerTail)
+      } else {
+        while (innerHead?.prev) {
+          innerHead = innerHead.prev
+        }
+
+        while (innerTail?.next) {
+          innerTail = innerTail.next
+        }
+      }
+    },
+    toArray() {
+      const arr: LinkNode[] = []
+      let currentItem = innerHead
+      while (currentItem?.next) {
+        const { prev, next, ...other } = currentItem
+        arr.push(other)
+        currentItem = next
+      }
+      if (currentItem) {
+        const { prev, ...other } = currentItem
+        arr.push(other)
+      }
+
+      return arr
+    },
+  }
+  if (initNode && !initNode.next && !initNode.prev) {
+    instance.unshift(initNode)
+  } else if (initNode) {
+    refreshHeadAndTail(initNode)
+    if (innerHead && innerHead === innerTail) {
+      innerTail = undefined
+      innerHead.next = undefined
+      innerHead.prev = undefined
+    }
   }
   return instance
+}
+
+export function isList(node: LinkNode) {
+  return !!(node.prev || node.next)
+}
+
+export function getItemById(inputArr: LinkNode[], id: number) {
+  for (const item of inputArr) {
+    if (item.id === id) {
+      return item
+    }
+  }
+}
+
+export function addItemBeforeId(inputArr: LinkNode[], inputItem: LinkNode, beforeId: number) {
+  const targetItem = getItemById(inputArr, beforeId)
+  if (!targetItem) return
+  const tmpPrev = targetItem.prev
+  inputItem.next = targetItem
+  targetItem.prev = inputItem
+  if (tmpPrev) {
+    tmpPrev.next = inputItem
+    inputItem.prev = tmpPrev
+  }
+  return targetItem
+}
+export function addItemAfterId(inputArr: LinkNode[], inputItem: LinkNode, afterId: number) {
+  const targetItem = getItemById(inputArr, afterId)
+  if (!targetItem) return
+  const tmpNext = targetItem.next
+  inputItem.prev = targetItem
+  targetItem.next = inputItem
+  if (tmpNext) {
+    tmpNext.prev = inputItem
+    inputItem.next = tmpNext
+  }
+  return targetItem
+}
+
+export function createOrderList(inputArr: LinkNode[]) {
+  const headList = createList()
+  const tailList = createList()
+  const currentList = createList()
+
+  inputArr.forEach((item) => {
+    if (item.first) {
+      if (isList(item)) {
+        const list = createList(item)
+        headList.unshiftList(list)
+      } else {
+        headList.unshift(item)
+      }
+      return
+    }
+
+    if (item.last) {
+      if (isList(item)) {
+        const list = createList(item)
+        tailList.pushList(list)
+      } else {
+        tailList.push(item)
+      }
+      return
+    }
+
+    if (item.before) {
+      const tItem = addItemBeforeId(inputArr, item, item.before)
+      if (tItem?.first) {
+        headList.refresh()
+      } else if (tItem?.last) {
+        tailList.refresh()
+      } else {
+        currentList.refresh()
+      }
+      return
+    }
+    if (item.after) {
+      const tItem = addItemAfterId(inputArr, item, item.after)
+      if (tItem?.first) {
+        headList.refresh()
+      } else if (tItem?.last) {
+        tailList.refresh()
+      } else {
+        currentList.refresh()
+      }
+      return
+    }
+
+    if (isList(item)) {
+      const list = createList(item)
+      currentList.pushList(list)
+    } else {
+      currentList.push(item)
+    }
+  })
+
+  return {
+    toArray() {
+      const list = createList()
+      list.pushList(headList)
+      list.pushList(currentList)
+      list.pushList(tailList)
+      return list.toArray()
+    },
+  }
 }
